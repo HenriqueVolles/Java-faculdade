33aabaad884c6df4dd622a0b550e0a944d57d2f8
HenriqueVolles <hevolles@gmail.com>
2025-05-27 15:48:37 -0300

    Update Uni5Exe19.java

--- Uni5Exe19.java
+++ Uni5Exe19.java
@@ -1,3 +1,5 @@
+//Uma loja de departamentos oferece aos seus clientes um determinado desconto de acordo com o valor da compra efetuada. O desconto é de 20% caso o valor da compra seja maior que R$ 500,00 e de 15% caso seja menor ou igual. Descreva um algoritmo que leia o valor da compra e escreva o total a pagar a cada compra. Informe também o valor total recebido pela loja ao final do dia. A leitura deve ser finalizada com a leitura 0 para o valor da compra.
+
 import java.util.Scanner;
 
 public class Uni5Exe19 {

054326d686f7bf14cf9016eea473b7170983f149
HenriqueVolles <hevolles@gmail.com>
2025-05-27 15:36:02 -0300

    Update Uni6Exe08.java

--- Uni6Exe08.java
+++ Uni6Exe08.java
@@ -1,3 +1,6 @@
+// Faça um programa que leia um valor N inteiro limitado a 20 posições. Com base neste valor, crie um vetor do tipo real. Faça o usuário informar valores para as posições deste vetor e coloque-as nas posições na sequência informada pelo usuário. Imprima uma tabela contendo cada valor diferente e o número de vezes que o valor aparece no vetor (veja exemplo a seguir).
+
+
 import java.util.Scanner;
 
 public class Uni6Exe08 {

e23527b7d0b440339b3b60dc75c9b0874b77c191
HenriqueVolles <hevolles@gmail.com>
2025-05-27 15:56:02 -0300

    Update Uni5Exe26.java

--- Uni5Exe26.java
+++ Uni5Exe26.java
@@ -1,3 +1,10 @@
+//Astolfo resolveu fazer uma viagem para conhecer o nosso país. Ele pretende visitar várias cidades, dividindo assim a viagem em vários trechos. Entretanto Astolfo se nega a passar por estradas cujo custo do pedágio excede um determinado valor. Faça um programa que leia inicialmente o valor do pedágio ao qual Astolfo se nega a pagar. A seguir leia várias duplas de valores representando respectivamente o custo do pedágio e a distância (em Km) do trecho. Calcular e escrever:
+
+//quantos trechos da viagem possuem um valor de pedágio acima do qual Astolfo negase a pagar;
+//quantos trechos foram informados;
+//quantos trechos acima de 150Km de distância possuem um valor de pedágio que Astolfo concorda em pagar.
+//OBS: O programa será encerrado ao ser fornecido um valor de pedágio negativo. Neste caso a leitura da distância não deve ser realizada. Os resultados devem ser impressos no final do programa.
+
 import java.util.Scanner;
 
 public class Uni5Exe26 {

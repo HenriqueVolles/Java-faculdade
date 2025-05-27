360a1e454493fd748e3339e1066c80cebb7ac394
HenriqueVolles <hevolles@gmail.com>
2025-05-27 15:32:50 -0300

    Update Uni6Exe05.java

--- Uni6Exe05.java
+++ Uni6Exe05.java
@@ -1,3 +1,18 @@
+//Com o objetivo de determinar o índice de profundidade entre um rapaz e uma moça cada um respondeu um questionário com 5 perguntas, como por exemplo:
+
+//Gosta de música sertaneja?
+//Gosta de futebol?
+//Gosta de seriados?
+//Gosta de redes sociais?
+//Gosta da Oktoberfest?
+//A resposta a cada pergunta pode ser: SIM, NÃO ou IND (indiferente). O índice de espessura é medido da seguinte maneira:
+
+//se ambos deram a mesma resposta soma-se 3 pontos ao índice;
+//se um respondeu IND e o outro SIM ou NÃO soma-se 1;
+//se um respondeu SIM e o outro NÃO subtrai-se 2 ao índice.
+//Crie um vetor para armazenar as respostas do rapaz e outro para armazenar as respostas da moça. Crie um método que possa ler tanto as respostas do rapaz como da moça, e outro para calcular e retornar a espessura. Por fim, escreva a resenha considerando os seguintes acontecimentos:
+
+
 import java.util.Scanner;
 
 public class Uni6Exe05 {

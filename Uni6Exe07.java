ffc5a0fc686a1361fc544168ef74bc5849111e26
HenriqueVolles <hevolles@gmail.com>
2025-05-27 15:34:47 -0300

    Update Uni6Exe07.java

--- Uni6Exe07.java
+++ Uni6Exe07.java
@@ -1,3 +1,10 @@
+//Faça um programa que leia um valor N inteiro limitado a 20 posições. Com base neste valor, crie um vetor do tipo inteiro. Em seguida, solicite ao usuário que digite um número inteiro várias vezes, até preencher o vetor. Esse número deverá ser armazenado no vetor caso ainda não exista, e se o valor já existir deve ser pedido um novo valor. Isto é, se o usuário informar o número 2 e já existir o número 2 em alguma posição do vetor, o número não deve ser adicionado. Após ter adicionado todos os números dentro do vetor, o mesmo deve ser ordenado. Para isso reordene os elementos internos do vetor de modo que este fique em ordem crescente, conforme no exemplo: OBSERVAÇÃO: Não utilize o Arrays.sort
+
+//Origem do vetor: [0][4][2][6][3]
+//Vetor ordenado: [0][2][3][4][6]
+//Crie um método para inserir os valores no vetor, outro para ordenar o vetor e outro para informar o vetor resultante. Para "método ordenar" você pode usar o "método bolha
+
+
 import java.util.Scanner;
 
 public class Uni6Exe07 {

e0e6ca13f54e0866ad3ab05dba30fc545f293df4
HenriqueVolles <hevolles@gmail.com>
2025-05-27 15:30:34 -0300

    Update Uni6Exe03.java

--- Uni6Exe03.java
+++ Uni6Exe03.java
@@ -1,3 +1,6 @@
+//Descreva um algoritmo que leia 12 valores reais e os coloque em um vetor de 12 posições do tipo real. Em seguida, modifique o vetor de modo que os valores das posições ímpares sejam aumentados em 5% e os valores das posições pares sejam aumentados em 2%. Imprima o vetor resultante. Faça um método para ler os valores, outro para ajustar os valores dentro do vetor e outro para escrever os valores atualizados do vetor.
+
+
 import java.util.Scanner;
 
 public class Uni6Exe03 {

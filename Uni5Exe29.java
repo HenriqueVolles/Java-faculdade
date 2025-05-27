832f9ab5325ad2532a09de46fa8b0c86201ca5da
HenriqueVolles <hevolles@gmail.com>
2025-05-27 15:59:14 -0300

    Update Uni5Exe29.java

--- Uni5Exe29.java
+++ Uni5Exe29.java
@@ -1,3 +1,7 @@
+//Uma caixa automática possui cédulas de 1, 2, 5, 10 e 20. Faça um programa que leia um valor e informe a quantidade mínima de cédulas que ele precisará combinar para entregar o valor solicitado. Por exemplo: se o valor for 6, então ele fornecerá uma cédula de 5 e outra de 1. Se o número for 47, então ele fornecerá duas cédulas de 20, uma de 5 e outras de 2.
+
+
+
 import java.util.Scanner;
 
 public class Uni5Exe29 {

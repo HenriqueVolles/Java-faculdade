767441b96827f6ba57672326e06fecc6a038cfea
HenriqueVolles <hevolles@gmail.com>
2025-05-27 16:03:33 -0300

    Update Uni5Exe32.java

--- Uni5Exe32.java
+++ Uni5Exe32.java
@@ -1,3 +1,5 @@
+//Escreva um programa que imprima um calendário para um determinado mês. O calendário deve conter cada dia do mês e o dia da semana correspondente. A entrada consiste de um inteiro especificando em que dia da semana cai o primeiro dia do mês (1=Domingo, 2=Segunda,...,7=Sábado) e um inteiro especificando o número de dias que o mês possui.
+
 import java.util.Scanner;
 public class Uni5Exe32 {
 	public static void main(String[] args) {

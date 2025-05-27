b79f43cc403ac4e2bd2ab122a8c5f27deba45c96
HenriqueVolles <hevolles@gmail.com>
2025-05-27 15:51:56 -0300

    Update Uni5Exe21.java

--- Uni5Exe21.java
+++ Uni5Exe21.java
@@ -1,3 +1,5 @@
+//Chico tem 1,50 metro e cresce 2 centímetros por ano, enquanto Zé tem 1,10 metros e cresce 3 centímetros por ano. Construa um algoritmo que calcule e imprima quantos anos serão necessários para que Zé seja maior que Chico.
+
 import java.util.Scanner;
 
 public class Uni5Exe21 {
